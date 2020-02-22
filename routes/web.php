ef9d5cf7e9253af6244c17f94bd65412894cd654
summeryio <summeryio@summeryiodeiMac.local>
2020-02-22 21:33:24 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 // 个人中心页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
