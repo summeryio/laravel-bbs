40130b84ffd1717c0b83ba25f63c4dcc64248d88
summeryio <summeryio@summeryiodeiMac.local>
2020-02-22 22:02:23 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 一个话题属于一个分类，$topic->category
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 一个话题属于一个作者， $topic->user
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
