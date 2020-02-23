089d50cb24717f3f4c099747ea307425ad407ff7
summeryio <summeryio@summeryiodeiMac.local>
2020-02-23 22:00:55 +0800

    帖子添加slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -11,8 +12,13 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
-        $topic->body = clean($topic->body, 'user_topic_body');
-        $topic->excerpt = make_excerpt($topic->body);
+        $topic->body = clean($topic->body, 'user_topic_body'); // XSS 过滤
+        $topic->excerpt = make_excerpt($topic->body); // 生成话题摘录
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 
     public function updating(Topic $topic)
