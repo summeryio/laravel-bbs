b37ac49adb61a4063a26cb1eb21cd465580b101b
summeryio <summeryio@summeryiodeiMac.local>
2020-02-24 19:18:34 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Handlers\SlugTranslateHandler;
 use App\Http\Requests\Request;
 use App\Models\Topic;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -15,10 +16,13 @@ class TopicObserver
     {
         $topic->body = clean($topic->body, 'user_topic_body'); // XSS 过滤
         $topic->excerpt = make_excerpt($topic->body); // 生成话题摘录
+    }
 
+    public function saved(Topic $topic) {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 
@@ -26,7 +30,8 @@ class TopicObserver
     {
         // 标题更新， 相应更新slug
         if ($topic->isDirty('title')) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
