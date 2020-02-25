9d6a6b02bfc19dc9635452b1fac2bf704c686a8f
summeryio <summeryio@summeryiodeiMac.local>
2020-02-25 09:38:27 +0800

    支持队列的邮件通知

--- app/Notifications/TopReplied.php
+++ app/Notifications/TopReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Reply;
 
-class TopReplied extends Notification
+class TopReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
