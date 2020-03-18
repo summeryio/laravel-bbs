e4c38d105a9ca8e286b7f74e91ce5c6b249e3529
summeryio <summeryio@summeryiodeiMac.local>
2020-03-18 15:31:05 +0800

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->namespace('Api')
     ->name('api.v1')
-    ->middleware('throttle:' . config('api.rate_limits.sign'))
+//    ->middleware('throttle:' . config('api.rate_limits.sign'))
     ->group(function () {
     // 短信验证码
     Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
 
     // 用户注册
     Route::post('users', 'UsersController@store')->name('users.store');
+
+    // 图片验证码
+    Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
 });
