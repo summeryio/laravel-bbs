c866b326eb92c7304fd58d01c5b1cfd7501cef64
summeryio <summeryio@summeryiodeiMac.local>
2020-03-20 10:47:07 +0800

    删除话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -24,4 +24,11 @@ class TopicsController extends Controller
 
         return new TopicResource($topic);
     }
+
+    public function destroy(Topic $topic) {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
+
+        return response(null, 204);
+    }
 }
